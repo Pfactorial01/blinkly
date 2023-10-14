22b3f35eb5063362218241334626c2192b69477f
Adebayo Adeoye <adeoyeadebayo18@gmail.com>
2023-10-14 18:53:30 +0100

    Implemented decode endpoint

--- src/services/urls.tsx
+++ src/services/urls.tsx
@@ -1,10 +1,25 @@
 "use server"
 
 const baseUrl = "http://localhost:3000/api"
+
 export const encodeUrl = async ({url}: {url: string}) => {
     const response = await fetch(`${baseUrl}/encode`, {
         method: 'POST',
+        cache: 'no-store',
         body: JSON.stringify(url)
+    });
+    const result = await response.json()
+    if (response.status !== 200) {
+        return { message: "error", data: result }
+    }
+    return { message: "success", data: result }
+}
+
+export const decodeUrl = async ({shortLink}: {shortLink: string}) => {
+    const response = await fetch(`${baseUrl}/decode`, {
+        method: 'POST',
+        cache: 'no-store',
+        body: JSON.stringify(shortLink)
     });
     const result = await response.json()
     if (response.status !== 200) {
