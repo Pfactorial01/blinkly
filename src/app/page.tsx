ecd6f31935a15187fb24289a5f84522efc00bfc9
Adebayo Adeoye <adeoyeadebayo18@gmail.com>
2023-10-16 01:57:31 +0100

    UI update for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@
 import { useForm } from "react-hook-form"
 import { useState, useTransition } from "react";
 import { encodeUrl } from "@blinkly/services/urls";
+import { toast } from "react-toastify";
+import { ClipboardIcon } from "@heroicons/react/24/outline";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export const dynamic = "force-dynamic";
 export default function Home() {
@@ -18,19 +21,21 @@ export default function Home() {
     startTransition(async () => {
       const response = await encodeUrl({url})
       if (response.message === "error") {
-        console.log(response)
+        toast.error(response.message)
       }
       const {data: { shortenedUrl }} = response
       setShortenedLink(shortenedUrl)
     });
   }
+
   return (
-    <div className="container flex flex-col items-center justify-center mx-auto h-full px-6">
-      <h1>Blinkly</h1>
-      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+    <div className="flex flex-col gap-5 md:gap-8 items-center justify-center bg-teal-700 rounded-md md:rounded-3xl shadow-2xl p-3 md:py-20">
+      <h1 className="text-4xl md:text-9xl font-extrabold">Blinkly</h1>
+      <form className="flex flex-col items-center justify-center gap-5 md:gap-8" onSubmit={handleSubmit(onSubmit)}>
         <input
-         className={`border focus: outline-none ${errors.url && `border-red-500`}`}
+         className={`border mx-2 md:mx-80 rounded-md py-1 px-3 w-80 md:w-[700px] focus: outline-none ${errors.url && `border-red-500`}`}
          type="text"
+         placeholder="Insert your link here"
          {...register("url", {
           required: true,
           pattern: {
@@ -39,12 +44,27 @@ export default function Home() {
           },
         })}
           />
-        {errors.url && <p>{errors.url.message}</p>}
-        <button type="submit">{isPending ? "Loading.." : "Shorten Link"}</button>
+        <button 
+          className="border md:border-[2px] border-slate-950 bg-yellow-100 shadow-2xl rounded-md w-32 md:w-40" 
+          type="submit" 
+          onClick={()=> {if (errors.url) toast.error(errors.url.message)}}
+          disabled={isPending}
+          >{isPending ? "Loading.." : "Shorten Link"}
+        </button>
       </form>
-      {shortenedLink && 
-        <div>
-          Here is your shortenedLink: {shortenedLink}
+      {isPending===false && shortenedLink && 
+        <div className="flex flex-col">
+          <div className="flex flex-row md:mr-3 p-1">
+            <h1>Here is your shortened link:</h1>
+          </div>
+           <div className="bg-white rounded-sm flex items-center justify-center">
+              <h1 className="mr-3">{shortenedLink}</h1>
+              <CopyToClipboard
+                text={shortenedLink}
+                onCopy={() => toast.success("Copied")}>
+                  <ClipboardIcon className="hover:cursor-pointer h-4 w-4" title="copy" />
+              </CopyToClipboard>
+            </div>
         </div>
       }
     </div>
